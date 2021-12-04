8bc34df7160a59dd2f03d4c33e87d7be317e602b
Abdullah5844 <95524714+Abdullah5844@users.noreply.github.com>
2021-12-04 23:57:23 +0500

    My_Project

---
+++ src/My_First_Project.java
@@ -0,0 +1,13 @@
+
+public class My_First_Project {
+
+	public static void main(String[] args) {
+		String name ="Abdullah";
+		String RNo="20i-0867";
+		
+		System.out.println(name);
+		System.out.println(RNo);
+
+	}
+
+}
